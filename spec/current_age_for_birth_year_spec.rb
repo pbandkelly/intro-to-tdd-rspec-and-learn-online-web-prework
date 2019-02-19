edd150b2c6d6e0683432af5e7dbd9880415be301
Kelly Solberg <kellysolberg@icloud.com>
2019-02-19 14:40:53 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -5,8 +5,9 @@ describe "current_age_for_birth_year method" do
     age_of_person = current_age_for_birth_year(1984)
 
     expect(age_of_person).to eq(19)
+    
+  end
+end
 def
 current_age_for_birth_year
-end
-  end
 end
